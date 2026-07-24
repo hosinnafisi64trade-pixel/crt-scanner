dd67fbd2bc0485922750f67952e4d08b22c7faaf
hosinnafisi64trade-pixel <hosinnafisi64.trade@gmail.com>
2026-07-24 13:40:16 +0330

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,24 @@
-print("CRT Scanner Bot Started")
+import requests
+import time
+
+print("CRT Scanner Started")
+
+BASE_URL = "https://api.toobit.com"
+
+def get_symbols():
+    url = BASE_URL + "/api/v1/futures/market/tickers"
+    r = requests.get(url, timeout=10)
+    data = r.json()
+
+    symbols = []
+    for item in data["data"]:
+        symbols.append(item["symbol"])
+
+    return symbols
+
+symbols = get_symbols()
+
+print(f"Found {len(symbols)} symbols")
+
+for symbol in symbols:
+    print(symbol)
